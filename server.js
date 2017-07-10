66bf1ae7e691dd7b6df6f96131e5b5b56c09dab9
jarontai <jaroncn@gmail.com>
2017-07-10 21:47:09 +0800

    feat: init sequelize/mysql

--- server.js
+++ server.js
@@ -3,10 +3,31 @@
 require('dotenv').config();
 
 const express = require('express');
+const Sequelize = require('sequelize');
 
 const app = express();
 const port = process.env.SERVER_PORT || 3000;
 
+// db
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST || 'localhost',
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  }
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+// routes
 app.get('/', (req, res) => {
   res.end('Hola!');
 });
