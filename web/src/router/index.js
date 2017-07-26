8a4f1dc16fa1ed6ba6aa6b8df54ea990a716afe9
daiqiu <daiqiu@appgame.com>
2017-07-26 10:52:26 +0800

    refactor(web): remove index component

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,15 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Index from '@/components/Index';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Index',
-      component: Index,
-    }
-  ],
+
+  ]
 });
