16a4e2ccf3d347e8301cc6c7d85716665cbb8be5
daiqiu <daiqiu@appgame.com>
2017-07-27 18:59:38 +0800

    feat(web): make navigator works

--- web/src/components/layout/Navigator.vue
+++ web/src/components/layout/Navigator.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="layout-breadcrumb">
     <Breadcrumb>
-      <Breadcrumb-item>首页</Breadcrumb-item>
-      <Breadcrumb-item>应用中心</Breadcrumb-item>
-      <Breadcrumb-item>某应用</Breadcrumb-item>
+      <Breadcrumb-item v-for="itemName in itemNames" :key="itemName">{{ itemName }}</Breadcrumb-item>
     </Breadcrumb>
   </div>
 </template>
 
 <script>
+import EventBus from '@/event_bus';
+import constant from '@/constant';
+
 export default {
   name: 'Navigator',
-  props: [],
-  computed: {
+  data() {
+    return {
+      itemNames: ['Dashboard']
+    };
+  },
+  created() {
+    // 路由改变更新导航
+    EventBus.$on('route-change', (data) => {
+      const toRoute = data.to;
+      const toPath = toRoute.path.substr(1);
+      const pathArr = toPath.split('/');
+      const itemNames = [];
+      let temp = '';
+      pathArr.forEach((path) => {
+        if (temp) {
+          temp += ':';
+        }
+        temp += path;
+        itemNames.push(constant.permissionMenuMap[temp] || 'unknown menu');
+      });
+      this.itemNames = itemNames;
+    });
   }
 };
 </script>
--- web/src/constant.js
+++ web/src/constant.js
@@ -2,8 +2,8 @@ export default {
   permissionMenuMap: {
     'dashboard': 'Dashboard',
     'admin': '后台管理',
-    'admin:user': '后台用户管理',
-    'admin:role': '后台角色管理',
-    'admin:permission': '后台权限管理'
+    'admin:user': '用户管理',
+    'admin:role': '角色管理',
+    'admin:permission': '权限管理'
   }
 };
